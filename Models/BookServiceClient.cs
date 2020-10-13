9239ccad8a900ed62f3f25ce5b7137e6426c4a6e
ntdat1597 <53183723+ntdat1597@users.noreply.github.com>
2020-10-13 21:31:09 +0700

    Add project files.

---
+++ Models/BookServiceClient.cs
@@ -0,0 +1,20 @@
+﻿using ConSummerMVC.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConSummerMVC.Models
+{
+    public class BookServiceClient
+    {
+        BookServiceClient client = new BookServiceClient();
+
+        public List<Book> getAllBook()
+        {
+            var list = (List < Book > client.GetBookList().toList());
+            return list;
+        }
+
+    }
+}
